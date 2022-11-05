67cf78d6770450efd0cb4c06e39c9cc0e37c4762
fence4 <116279831+fence4@users.noreply.github.com>
2022-11-05 18:07:29 -0500

    Update theme.css.ts

--- src/theme.css.ts
+++ src/theme.css.ts
@@ -47,7 +47,7 @@ const fontWeights = {
 
 const fonts = {
   text: '"DM Sans", sans-serif',
-  heading: '"DM Sans", sans-serif',
+  heading: '"DM Mono", monospace',
   mono: "DM Mono, Menlo, monospace",
 }
 
