d26b9a7427860f6f87fe009d2726db2697525936
fence4 <116279831+fence4@users.noreply.github.com>
2022-11-05 18:01:48 -0500

    Update theme.css.ts

--- src/theme.css.ts
+++ src/theme.css.ts
@@ -47,7 +47,7 @@ const fontWeights = {
 
 const fonts = {
   text: '"DM Sans", sans-serif',
-  heading: '"DM Ariel", sans-serif',
+  heading: '"DM Sans", sans-serif',
   mono: "DM Mono, Menlo, monospace",
 }
 
